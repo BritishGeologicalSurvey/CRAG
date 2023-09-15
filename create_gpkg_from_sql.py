ceb5923b7a6af75c24c18a02ed7ec9b73437258d
John A Stevenson <jostev@bgs.ac.uk>
2023-09-15 14:02:26 +0100

    Add script for empty geopackage

--- create_gpkg_from_sql.py
+++ create_gpkg_from_sql.py
@@ -2,22 +2,23 @@ from pathlib import Path
 import sqlite3
 
 WORKDIR = Path.cwd()
+DB_FILE = WORKDIR / 'field-data-capture.gpkg'
 
-sql_file = WORKDIR / 'field-data-capture.gpkg.sql'
-db_file = WORKDIR / 'field-data-capture.gpkg'
 
-if db_file.exists():
-    db_file.unlink()
+def main():
+    sql_scripts = Path(WORKDIR / 'sql').glob('*.sql')
 
-print(db_file)
+    for sql_script in sql_scripts:
+        apply_script(DB_FILE, sql_script)
 
-with sqlite3.connect(db_file) as conn:
-    cursor = conn.cursor()
 
-    # Recreate the database from the dump file
-    cursor.executescript(sql_file.read_text())
+def apply_script(geopackage_file, sql_script):
+    with sqlite3.connect(geopackage_file) as conn:
+        cursor = conn.cursor()
 
-    # Confirm some data
-    cursor.execute("SELECT fid, uuid, description FROM locality_point")
+        # Recreate the database from the dump file
+        cursor.executescript(sql_script.read_text())
 
-    print(list(cursor.fetchall()))
+
+if __name__ == "__main__":
+    main()
