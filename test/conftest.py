f6603a6887bcece275ae97f8546aebdb35aeb02d
Leo Rudczenko <leorud@bgs.ac.uk>
2023-09-20 14:25:47 +0100

    Add column constraints test

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from pathlib import Path
 
 import pytest
@@ -20,3 +21,26 @@ def data_model_gpkg(tmp_path: Path):
     # Close database and delete geopackage file
     conn.close()
     db_file.unlink()
+
+
+@pytest.fixture()
+def locality_manmade_landform_dict_row():
+    return {"fid": 1, "objectid": None, "activity_fuid": None, "locality_fuid": "a", "uuid": "abc",
+            "manmade_type_code": "b", "dip": 45, "dip_dir": 180, "length": None, "width": None, "comment": None,
+            "user_entered": "c", "date_entered": dt.datetime.now(), "user_updated": None, "date_updated": None}
+
+
+@pytest.fixture()
+def locality_structural_measurement_dict_row():
+    return {"fid": 1, "objectid": None, "activity_fuid": None, "locality_fuid": "a", "uuid": "abc",
+            "structure_type_category": "b", "structure_type_code": "c", "dip": 45, "dip_direction": 180,
+            "secondary_attrib": None, "third_attrib": None, "user_entered": "d",
+            "date_entered": dt.datetime.now(), "user_updated": None, "date_updated": None}
+
+
+@pytest.fixture()
+def locality_superficial_landform_dict_row():
+    return {"fid": 1, "objectid": None, "activity_fuid": None, "locality_fuid": "a", "uuid": "abc",
+            "superficial_type_category": "b", "superficial_type_code": "c", "dip": 45, "length": None, "width": None,
+            "height_depth": None, "comment": None, "user_entered": "d", "date_entered": dt.datetime.now(),
+            "user_updated": None, "date_updated": None}
--- test/test_data_model.py
+++ test/test_data_model.py
@@ -1,3 +1,8 @@
+from typing import (
+    Optional,
+    Any,
+)
+
 import pytest
 import etlhelper as etl
 
@@ -22,10 +27,10 @@ import etlhelper as etl
         ),
     ],
 )
-def test_data_model_columns(data_model_gpkg, tables, expected_col_names):
+def test_data_model_columns(data_model_gpkg, tables: list[str], expected_col_names: list[str]):
     for table in tables:
         table_info = etl.table_info(table=table, conn=data_model_gpkg)
-        # Get a list of only the columns we are checking for
+        # Only get the columns from the actual list of columns that we want to check
         check_cols = [col for col in table_info if col.name in expected_col_names]
 
         check_col_names = [col.name for col in check_cols]
@@ -33,6 +38,54 @@ def test_data_model_columns(data_model_gpkg, tables, expected_col_names):
         check_col_names.sort()
         expected_col_names.sort()
         assert check_col_names == expected_col_names
+
+
+@pytest.mark.parametrize(
+    ["table", "new_data", "expected_string"],
+    [
+        # Table: locality_manmade_landform
+        ("locality_manmade_landform", {"dip": 0}, None),
+        ("locality_manmade_landform", {"dip": 90}, None),
+        ("locality_manmade_landform", {"dip": -1}, "CHECK constraint failed: dip"),
+        ("locality_manmade_landform", {"dip": 91}, "CHECK constraint failed: dip"),
+
+        # Table: locality_structural_measurement
+        ("locality_structural_measurement", {"dip": 0}, None),
+        ("locality_structural_measurement", {"dip": 90}, None),
+        ("locality_structural_measurement", {"dip": -1}, "CHECK constraint failed: dip"),
+        ("locality_structural_measurement", {"dip": 91}, "CHECK constraint failed: dip"),
+        ("locality_structural_measurement", {"dip_direction": 0}, None),
+        ("locality_structural_measurement", {"dip_direction": 360}, None),
+        ("locality_structural_measurement", {"dip_direction": -1}, "CHECK constraint failed: dip_direction"),
+        ("locality_structural_measurement", {"dip_direction": 361}, "CHECK constraint failed: dip_direction"),
+
+        # Table: locality_superficial_landform
+        ("locality_superficial_landform", {"dip": 0}, None),
+        ("locality_superficial_landform", {"dip": 90}, None),
+        ("locality_superficial_landform", {"dip": -1}, "CHECK constraint failed: dip"),
+        ("locality_superficial_landform", {"dip": 91}, "CHECK constraint failed: dip"),
+    ],
+)
+def test_data_model_columns_constraints(
+    request,
+    data_model_gpkg,
+    table: str,
+    new_data: dict[str, Any],
+    expected_string: Optional[str],
+):
+    # To use pytest fixtures with parameterisation, you have to access the fixture via request with it's string name
+    row = request.getfixturevalue(table + "_dict_row")
+    row.update(new_data)
+    rows = [row]
+
+    if expected_string is not None:
+        # Check that the correct error is raised
+        with pytest.raises(etl.exceptions.ETLHelperInsertError) as excinfo:
+            etl.load(table=table, conn=data_model_gpkg, rows=rows)
+        assert expected_string in str(excinfo.value)
+    else:
+        # Inserting the row should not raise an error
+        etl.load(table=table, conn=data_model_gpkg, rows=rows)
 
 
 def test_gpkg_contents(data_model_gpkg):
@@ -72,4 +125,5 @@ def test_gpkg_contents(data_model_gpkg):
     """
     actual_contents = etl.fetchall(query, conn=data_model_gpkg, row_factory=etl.row_factories.list_row_factory)
 
+    # Assert
     assert actual_contents == expected_contents
