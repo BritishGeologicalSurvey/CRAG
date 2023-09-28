ceb2f6bab9262f12576e781393f15417f71abcfe
Leo Rudczenko <leorud@bgs.ac.uk>
2023-09-28 16:28:02 +0100

    Update script to reflect newest version

--- plugin/create_gpkg_from_sql.py
+++ plugin/create_gpkg_from_sql.py
@@ -3,7 +3,7 @@ import logging
 import sqlite3
 
 logger = logging.getLogger('create_gpkg')
-logger.setLevel(logging.INFO)
+# Using __file__ rather than cwd() so that the import location does not affect the path
 WORKDIR = Path(__file__).parent
 DB_FILE = WORKDIR / 'field-data-capture.gpkg'
 
