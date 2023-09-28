af9856157f47ceac19120c40402b405969829066
Leo Rudczenko <leorud@bgs.ac.uk>
2023-09-28 15:46:45 +0100

    Add initial QGIS plugin

---
+++ plugin/create_gpkg_from_sql.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import logging
+import sqlite3
+
+logger = logging.getLogger('create_gpkg')
+logger.setLevel(logging.INFO)
+WORKDIR = Path(__file__).parent
+DB_FILE = WORKDIR / 'field-data-capture.gpkg'
+
+
+def main(
+    workdir: Path = WORKDIR,
+    db_file: Path = DB_FILE,
+):
+    if db_file.exists():
+        logger.info('Deleting existing database')
+        db_file.unlink()
+
+    sql_scripts = Path(workdir / 'sql').glob('V*.sql')
+
+    for sql_script in sorted(sql_scripts):
+        logger.info('Applying %s', sql_script.name)
+        apply_script(db_file, sql_script)
+
+
+def apply_script(geopackage_file: Path, sql_script: Path):
+    with sqlite3.connect(geopackage_file) as conn:
+        cursor = conn.cursor()
+
+        # Recreate the database from the dump file
+        cursor.executescript(sql_script.read_text())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
