200f939691442f35cefe147cc3c52a9887c55471
John A Stevenson <jostev@bgs.ac.uk>
2023-10-11 14:38:15 +0100

    Register view geometry columns with geopackage

--- plugin/sql/V006__locality_point_views.sql
+++ plugin/sql/V006__locality_point_views.sql
@@ -23,7 +23,10 @@ CREATE VIEW IF NOT EXISTS "view_structural_measurement" AS
 ;
 
 INSERT INTO gpkg_contents
-VALUES('view_structural_measurement','features','view_structural_measurement','View with structural measurements at locality positions','2023-09-15T13:21:52.679Z',NULL,NULL,NULL,NULL,NULL);
+VALUES('view_structural_measurement','features','view_structural_measurement','View with structural measurements at locality positions','2023-09-15T13:21:52.679Z',NULL,NULL,NULL,NULL,27700);
+
+INSERT INTO gpkg_geometry_columns
+VALUES('view_structural_measurement','geometry','POINT',27700,1,0);
 
 
 CREATE VIEW IF NOT EXISTS "view_exposure" AS
@@ -48,6 +51,9 @@ CREATE VIEW IF NOT EXISTS "view_exposure" AS
 ;
 
 INSERT INTO gpkg_contents
-VALUES('view_exposure','features','view_exposure','View with exposure results at locality positions','2023-09-15T13:21:52.679Z',NULL,NULL,NULL,NULL,NULL);
+VALUES('view_exposure','features','view_exposure','View with exposure results at locality positions','2023-09-15T13:21:52.679Z',NULL,NULL,NULL,NULL,27700);
+
+INSERT INTO gpkg_geometry_columns
+VALUES('view_exposure','geometry','POINT',27700,1,0);
 
 COMMIT;
