3742b4c924f9ecfcc86c819945461382e3174f0e
John A Stevenson <jostev@bgs.ac.uk>
2023-10-11 13:32:29 +0100

    Include views in ER diagram

--- plugin/create_er_diagram.py
+++ plugin/create_er_diagram.py
@@ -2,6 +2,7 @@ from eralchemy import render_er
 from sqlalchemy import (
     MetaData,
     create_engine,
+    event
 )
 
 from plugin.config import TABLE_LIST
@@ -12,8 +13,20 @@ def main(
     img_filepath: str = "er-diagram.png",
 ):
     engine = create_engine(f"sqlite:///{gpkg_filepath}")
+
+    # Define a hook to run as soon as engine connects.
+    # This one loads the Spatialite extension to all spatial functions to be used.
+    # https://docs.sqlalchemy.org/en/14/core/engines.html#modifying-the-dbapi-connection-after-connect-or-running-commands-after-connect
+    # See also https://stackoverflow.com/a/73451804/3508733
+    @event.listens_for(engine, "connect")
+    def connect(conn, _):
+        conn.enable_load_extension(True)
+        conn.execute("SELECT load_extension('mod_spatialite');")
+        conn.enable_load_extension(False)
+
+    # Connect and read metadata
     meta = MetaData()
-    meta.reflect(bind=engine)
+    meta.reflect(bind=engine, views=True)
 
     # Only include the given tables in the diagram
     new_meta = MetaData()
