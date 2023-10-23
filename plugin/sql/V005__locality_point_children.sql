99c42309d125b4a9e630cb2825829ac30c6f3599
John Stevenson - BGS <jostev@bgs.ac.uk>
2023-10-23 16:32:57 +0000

    Reference category tables directly
    Although the "category" column in the "code" tables contains the values that we want, they are not unique and so an error is raised if you try to use them as a primary key.

--- plugin/sql/V005__locality_point_children.sql
+++ plugin/sql/V005__locality_point_children.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS "structural_measurement" (
   "user_updated" TEXT,
   "date_updated" DATETIME,
   FOREIGN KEY("structure_type_code") REFERENCES "dic_structure_code"("code"),
-  FOREIGN KEY("structure_type_category") REFERENCES "dic_structure_code"("category"),
+  FOREIGN KEY("structure_type_category") REFERENCES "dic_structure_category"("code"),
   FOREIGN KEY("locality_fuid") REFERENCES "locality_point"("uuid"),
   PRIMARY KEY("fid" AUTOINCREMENT)
 );
@@ -148,7 +148,7 @@ CREATE TABLE IF NOT EXISTS "superficial_landform" (
   "user_updated" TEXT,
   "date_updated" DATETIME,
   FOREIGN KEY("superficial_type_code") REFERENCES "dic_superficial_code"("code"),
-  FOREIGN KEY("superficial_type_category") REFERENCES "dic_superficial_code"("category"),
+  FOREIGN KEY("superficial_type_category") REFERENCES "dic_superficial_category"("code"),
   FOREIGN KEY("locality_fuid") REFERENCES "locality_point"("uuid"),
   PRIMARY KEY("fid" AUTOINCREMENT)
 );
