46ad6c7922dfde53571079bf3ee368d3ca1a610d
John A Stevenson <jostev@bgs.ac.uk>
2023-11-01 17:40:27 +0000

    Set CRS for views to EPSG:4326

--- plugin/sql/V006__locality_point_views.sql
+++ plugin/sql/V006__locality_point_views.sql
@@ -24,10 +24,10 @@ CREATE VIEW IF NOT EXISTS "view_structural_measurement" AS
 ;
 
 INSERT INTO gpkg_contents
-VALUES('view_structural_measurement','features','view_structural_measurement','View with structural measurements at locality positions','2023-09-15T13:21:52.679Z',NULL,NULL,NULL,NULL,27700);
+VALUES('view_structural_measurement','features','view_structural_measurement','View with structural measurements at locality positions','2023-09-15T13:21:52.679Z',NULL,NULL,NULL,NULL,4326);
 
 INSERT INTO gpkg_geometry_columns
-VALUES('view_structural_measurement','geometry','POINT',27700,1,0);
+VALUES('view_structural_measurement','geometry','POINT',4326,1,0);
 
 
 CREATE VIEW IF NOT EXISTS "view_exposure" AS
@@ -53,9 +53,9 @@ CREATE VIEW IF NOT EXISTS "view_exposure" AS
 ;
 
 INSERT INTO gpkg_contents
-VALUES('view_exposure','features','view_exposure','View with exposure results at locality positions','2023-09-15T13:21:52.679Z',NULL,NULL,NULL,NULL,27700);
+VALUES('view_exposure','features','view_exposure','View with exposure results at locality positions','2023-09-15T13:21:52.679Z',NULL,NULL,NULL,NULL,4326);
 
 INSERT INTO gpkg_geometry_columns
-VALUES('view_exposure','geometry','POINT',27700,1,0);
+VALUES('view_exposure','geometry','POINT',4326,1,0);
 
 COMMIT;
