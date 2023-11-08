384b28370b05ab26fbbb866ae9e00811a5f217d9
Leo Rudczenko <leorud@bgs.ac.uk>
2023-11-08 15:37:44 +0000

    Add test for add geopackage to project

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -3,7 +3,14 @@ These are tests for the plugin which depend on a running QGIS version which is s
 """
 from pathlib import Path
 
-from plugin.field_data_capture import FieldDataCapture
+import etlhelper as etl
+
+from conftest import setup_db_conn
+from plugin.config import TABLE_LIST
+from plugin.field_data_capture import (
+    FieldDataCapture,
+    gpkg_from_sql,
+)
 
 
 def test_instantiation(fdc):
@@ -14,6 +21,26 @@ def test_project_fixture(fdc: FieldDataCapture, qgs_project: Path):
     # Check the project directory
     assert fdc.project_dir.name == "test_project_dir"
     # Check the qgz file
-    files = list(fdc.project_dir.glob("*.qgz"))
+    files = list(fdc.project_dir.glob("*"))
     assert len(files) == 1
     assert files[0].name == "test_project.qgz"
+
+
+def test_add_gpkg_to_project(fdc: FieldDataCapture, qgs_project: Path):
+    # Act
+    # Directly call the backend method rather than the front end method to avoid message boxes
+    gpkg_from_sql(db_file=fdc.db_file)
+
+    # Check file exists
+    assert (fdc.project_dir / fdc.gpkg_filename).exists()
+
+    # Check tables are in file
+    conn = setup_db_conn(fdc.db_file)
+    table_rows = etl.fetchall(
+        "SELECT name FROM sqlite_schema",
+        conn,
+        row_factory=etl.row_factories.list_row_factory,
+    )
+    all_table_names = {row[0] for row in table_rows}
+    expected_table_names = set(TABLE_LIST)
+    assert expected_table_names.issubset(all_table_names)
