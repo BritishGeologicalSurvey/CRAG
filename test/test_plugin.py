0898f032dedfa447a65e61bbb4a0375fe0420a72
Leo Rudczenko <leorud@bgs.ac.uk>
2023-11-08 16:09:55 +0000

    Add test for add geopackage layers to project

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -4,6 +4,10 @@ These are tests for the plugin which depend on a running QGIS version which is s
 from pathlib import Path
 
 import etlhelper as etl
+from qgis.core import (
+    QgsLayerTreeGroup,
+    QgsProject,
+)
 
 from conftest import setup_db_conn
 from plugin.config import TABLE_LIST
@@ -27,6 +31,12 @@ def test_project_fixture(fdc: FieldDataCapture, qgs_project: Path):
     assert files[0].name == "test_project.qgz"
 
 
+def test_validation_good(fdc: FieldDataCapture, qgs_project: Path):
+    gpkg_from_sql(db_file=fdc.db_file)
+    assert fdc.project_is_active()
+    assert fdc.db_file.exists()
+
+
 def test_add_gpkg_to_project(fdc: FieldDataCapture, qgs_project: Path):
     # Act
     # Directly call the backend method rather than the front end method to avoid message boxes
@@ -45,3 +55,31 @@ def test_add_gpkg_to_project(fdc: FieldDataCapture, qgs_project: Path):
     all_table_names = {row[0] for row in table_rows}
     expected_table_names = set(TABLE_LIST)
     assert expected_table_names.issubset(all_table_names)
+
+
+def test_add_gpkg_layers_to_project(fdc: FieldDataCapture, qgs_project: Path):
+    # Arrange
+    gpkg_from_sql(db_file=fdc.db_file)
+    expected_root_names = {"locality_point", "views", "locality_data", "metadata"}
+
+    # Act
+    fdc.add_gpkg_layers_to_project()
+
+    # Assert
+    # Check root layers
+    root_layers = QgsProject.instance().layerTreeRoot().children()
+    root_names = {layer.name() for layer in root_layers}
+    assert expected_root_names == root_names
+
+    # Get only groups from the root
+    root_groups: list[QgsLayerTreeGroup] = [
+        layer
+        for layer in root_layers
+        if isinstance(layer, QgsLayerTreeGroup)
+    ]
+
+    # Check layers in groups
+    group_structure = fdc.get_groups_layers()
+    for group in root_groups:
+        group_layer_names = {layer.name() for layer in group.children()}
+        assert set(group_structure[group.name()]) == group_layer_names
