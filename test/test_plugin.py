300f77d8f62c66707745b886abecf6eaa3bb69fd
Leo Rudczenko <leorud@bgs.ac.uk>
2024-01-15 16:26:52 +0000

    Add tests for quick locality point

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -4,6 +4,7 @@ These are tests for the plugin which depend on a running QGIS version which is s
 import os
 import pwd
 from pathlib import Path
+from typing import Callable
 from unittest.mock import Mock
 from xml.dom import minidom
 
@@ -14,6 +15,7 @@ from qgis.core import (
     QgsProject,
     QgsVectorLayerUtils,
 )
+from qgis.PyQt.QtCore import pyqtBoundSignal
 
 from conftest import setup_db_conn
 from plugin.config import TABLE_LIST
@@ -297,12 +299,123 @@ def test_auto_increment_locality_point_name(
     layer = QgsProject.instance().mapLayersByName("locality_point")[0]
     for expected_name in expected_locality_point_names:
         layer.startEditing()
+        # Create a new feature with automatically generated values from the layer
         feature = QgsVectorLayerUtils.createFeature(layer)
         # Set the field_project_fuid to be the uuid of the field project from the test data set
-        feature.setAttribute(feature.fieldNameIndex("field_project_fuid"), "{d57614a8-21ba-47a5-8cb6-82c0b009ec1b}")
-        feature.setAttribute(feature.fieldNameIndex("exposure_type_code"), "AUGER_BOREHOLE")
+        feature.setAttribute("field_project_fuid", "{d57614a8-21ba-47a5-8cb6-82c0b009ec1b}")
+        feature.setAttribute("exposure_type_code", "AUGER_BOREHOLE")
         layer.addFeature(feature)
         layer.commitChanges()
 
         # Assert
         assert feature.attribute("name") == expected_name
+
+
+def test_quick_locality_point_enable(
+    fdc: FieldDataCapture,
+    qgs_project: Path,
+):
+    # Arrange
+    fdc.add_gpkg_to_project()
+    fdc.add_gpkg_layers_to_project()
+    fdc.add_test_data_to_project()
+    layer = QgsProject.instance().mapLayersByName("locality_point")[0]
+
+    # Act
+    # Enable quick locality point mode
+    fdc.toggle_quick_locality_point_mode()
+
+    # Assert
+    # We cannot check the active layer as it is not a working function in the mocked iface
+    assert layer.isEditable()
+    assert len(fdc.locality_point_slots) == 2
+    for signal, slot in fdc.locality_point_slots:
+        assert isinstance(signal, pyqtBoundSignal)
+        assert isinstance(slot, Callable)
+    assert fdc.quick_locality_point_mode
+
+
+def test_quick_locality_point_disable(
+    fdc: FieldDataCapture,
+    qgs_project: Path,
+):
+    # Arrange
+    fdc.add_gpkg_to_project()
+    fdc.add_gpkg_layers_to_project()
+    fdc.add_test_data_to_project()
+    # Enable quick locality point mode
+    fdc.toggle_quick_locality_point_mode()
+    layer = QgsProject.instance().mapLayersByName("locality_point")[0]
+
+    # Act
+    # Disable quick locality point mode
+    fdc.toggle_quick_locality_point_mode()
+
+    # Assert
+    assert not layer.isEditable()
+    assert fdc.locality_point_slots == []
+    assert not fdc.quick_locality_point_mode
+
+
+def test_quick_locality_point_add(
+    fdc: FieldDataCapture,
+    qgs_project: Path,
+    monkeypatch: pytest.MonkeyPatch,
+):
+    # Arrange
+    fdc.add_gpkg_to_project()
+    fdc.add_gpkg_layers_to_project()
+    fdc.add_test_data_to_project()
+    # Enable quick locality point mode
+    fdc.toggle_quick_locality_point_mode()
+    layer = QgsProject.instance().mapLayersByName("locality_point")[0]
+
+    # Monkeypatch the iface.openFeatureForm function to ensure it was called
+    mock_function = Mock()
+    monkeypatch.setattr(fdc.iface, "openFeatureForm", mock_function)
+
+    # Act 1
+    # Add a new locality_point feature
+    # Create a new feature with automatically generated values from the layer
+    feature_1 = QgsVectorLayerUtils.createFeature(layer)
+    # Set the field_project_fuid to be the uuid of the field project from the test data set
+    feature_1.setAttribute("field_project_fuid", "{d57614a8-21ba-47a5-8cb6-82c0b009ec1b}")
+    feature_1.setAttribute("exposure_type_code", "AUGER_BOREHOLE")
+    layer.addFeature(feature_1)
+    # Emit the GUI signal that triggers the auto save of the new feature
+    layer.editCommandEnded.emit()
+
+    # Assert 1
+    expected_fid_1 = 3
+    new_feature_1 = list(layer.getFeatures())[-1]
+    assert new_feature_1.attribute("fid") == expected_fid_1
+    assert fdc.quick_locality_point_fid == expected_fid_1
+    mock_function.assert_called_with(layer, new_feature_1)
+
+    # Act 2
+    # Add another new locality_point feature
+    # Create a new feature with automatically generated values from the layer
+    feature_2 = QgsVectorLayerUtils.createFeature(layer)
+    # Set the field_project_fuid to be the uuid of the field project from the test data set
+    feature_2.setAttribute("field_project_fuid", "{d57614a8-21ba-47a5-8cb6-82c0b009ec1b}")
+    feature_2.setAttribute("exposure_type_code", "AUGER_BOREHOLE")
+    layer.addFeature(feature_2)
+    # Emit the GUI signal that triggers the auto save of the new feature
+    layer.editCommandEnded.emit()
+
+    # Assert 2
+    expected_fid_2 = 4
+    new_feature_2 = list(layer.getFeatures())[-1]
+    assert new_feature_2.attribute("fid") == expected_fid_2
+    assert fdc.quick_locality_point_fid == expected_fid_2
+    mock_function.assert_called_with(layer, new_feature_2)
+
+    # Act 3
+    # Disable quick locality point mode
+    fdc.toggle_quick_locality_point_mode()
+
+    # Assert 3
+    assert not layer.isEditable()
+    assert fdc.locality_point_slots == []
+    assert not fdc.quick_locality_point_mode
+    assert fdc.quick_locality_point_fid is None
