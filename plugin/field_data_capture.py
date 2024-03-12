7a736037ad3b29e01610a5a43633c5dfb6e8bec9
Leo Rudczenko <leorud@bgs.ac.uk>
2024-03-12 12:21:36 +0000

    Flake8 fix

--- plugin/field_data_capture.py
+++ plugin/field_data_capture.py
@@ -943,11 +943,7 @@ class FieldDataCapture:
         Toggle the given quick locality point mode.
         Returns a boolean indicating the success of the process.
         """
-        if (
-            not self.validate_qgis_state(project_active=True, db_file_exists=True, fdc_layers_exist=True)
-            or
-            self.warn_unsaved_locality_children(parent=True)
-        ):
+        if not self.validate_qgis_state(project_active=True, db_file_exists=True, fdc_layers_exist=True) or self.warn_unsaved_locality_children(parent=True):  # noqa
             # Disable any current modes to prevent issues
             self.disable_quick_locality_mode()
             self.untoggle_quick_locality_buttons()
