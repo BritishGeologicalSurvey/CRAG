215cd90b00aaf28ca55151ecb99bb88f90feb8af
John A Stevenson <jostev@bgs.ac.uk>
2024-04-26 17:47:52 +0100

    flake8 fix

--- test/conftest.py
+++ test/conftest.py
@@ -100,7 +100,8 @@ def fdc(monkeypatch: pytest.MonkeyPatch) -> Generator[FieldDataCapture, None, No
     monkeypatch.setattr(iface, "cadDockWidget", lambda *args: cadDockWidget)
 
     # Apply monkeypatch for getting plugin metadata in QuickMapTools
-    monkeypatch.setattr(QuickMapToolBase, "get_plugin_metadata", lambda *args: {"version_installed": "fdc_test_fixture"})
+    monkeypatch.setattr(QuickMapToolBase, "get_plugin_metadata",
+                        lambda *args: {"version_installed": "fdc_test_fixture"})
 
     field_data_capture.initGui()
 
