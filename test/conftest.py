391f71b3584178c8d1412364e3f94aee03580e86
John A Stevenson <jostev@bgs.ac.uk>
2024-04-26 17:42:39 +0100

    Set qgis_plugin_version for local and test environments

--- test/conftest.py
+++ test/conftest.py
@@ -100,7 +100,7 @@ def fdc(monkeypatch: pytest.MonkeyPatch) -> Generator[FieldDataCapture, None, No
     monkeypatch.setattr(iface, "cadDockWidget", lambda *args: cadDockWidget)
 
     # Apply monkeypatch for getting plugin metadata in QuickMapTools
-    monkeypatch.setattr(QuickMapToolBase, "get_plugin_metadata", lambda *args: {"version_installed": "0.1"})
+    monkeypatch.setattr(QuickMapToolBase, "get_plugin_metadata", lambda *args: {"version_installed": "fdc_test_fixture"})
 
     field_data_capture.initGui()
 
