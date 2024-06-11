8c1ae4db9840275d6e28d81d93772fbf6f863de8
Leo Rudczenko <leorud@bgs.ac.uk>
2024-06-11 13:38:29 +0100

    Add bad copy test

--- test/test_copy_project_data.py
+++ test/test_copy_project_data.py
@@ -157,3 +157,61 @@ def test_copy_project_data_good(
     # Check that the photo files have been copied across
     for photo_file in (src_fdc_project / "photos").glob("*[!.placeholder]"):
         assert (dest_fdc_project / "photos" / photo_file.name).exists()
+
+
+def test_copy_project_data_bad(
+    src_fdc_project: Path,
+    dest_fdc_project: Path,
+):
+    # Arrange
+    expected_row_counts = {
+        "artificial_line": 1,
+        "bedrock_line": 0,
+        "field_project": 1,
+        "lithology": 2,
+        "locality_point": 2,
+        "manmade_landform": 0,
+        "mass_move_line": 0,
+        "media": 0,
+        "photo": 2,
+        "sample": 0,
+        "structural_measurement": 0,
+        "superficial_landform": 0,
+        "superficial_line": 0,
+        "terrain_line": 0,
+    }
+    expected_field_project_notes = "These are some empty notes honk"
+    # Change one of the locality_point names to match one of the test points
+    # This will cause the copy to fail as it does not abide by the UNIQUE constraint
+    dest_db = dest_fdc_project / "field-data-capture.gpkg"
+    with setup_db_conn(dest_db) as conn:
+        etl.execute("UPDATE locality_point SET name='test_point_002' WHERE name='leorudczenko_002'", conn)
+
+    # Act
+    copy_project_data = CopyProjectData(src_fdc_project, dest_fdc_project)
+    copy_project_data.copy_project_data()
+
+    # Assert
+    with sqlite3.connect(dest_db) as conn:
+
+        # Check that there are the correct number of rows per table in the destination database
+        # None should have been added
+        for table, expected_row_count in expected_row_counts.items():
+            row_count = etl.fetchone(
+                f"SELECT COUNT() AS count FROM {table}",
+                conn,
+                row_factory=etl.row_factories.tuple_row_factory,
+            )[0]
+            assert row_count == expected_row_count
+
+        # Check that the field project notes have not been changed
+        field_project_notes = etl.fetchone(
+            "SELECT notes FROM field_project",
+            conn,
+            row_factory=etl.row_factories.tuple_row_factory,
+        )[0]
+        assert field_project_notes == expected_field_project_notes
+
+    # Check that the photo files have not been copied across
+    for photo_file in (src_fdc_project / "photos").glob("*[!.placeholder]"):
+        assert not (dest_fdc_project / "photos" / photo_file.name).exists()
