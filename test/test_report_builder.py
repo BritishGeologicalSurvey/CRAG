814f7224994d371766ec7003520ef776c10c4918
Colin Blackburn <colb@bgs.ac.uk>
2024-06-13 14:36:48 +0100

    Add minimal test for children

--- test/test_report_builder.py
+++ test/test_report_builder.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 from conftest import locality_point_count
 
+from plugin.config import LOCALITY_POINT_CHILDREN
 from plugin.field_data_capture import FieldDataCapture
 from plugin.report_builder import ReportBuilder
 from plugin.utils import ipdb_breakpoint  # noqa
@@ -40,3 +41,6 @@ def test_get_report_data(fdc_project: FieldDataCapture, qgs_project: Path):
     # Check that the correct amount of data has been obtained
     row_count = locality_point_count(fdc_project)
     assert len(report_data['locality_points']) == row_count
+    for locality in report_data['locality_points'].values():
+        # Check that the children dict has been created
+        assert set(LOCALITY_POINT_CHILDREN) == set(locality['children'].keys())
