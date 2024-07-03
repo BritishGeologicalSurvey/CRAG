bc42c1566c3c4721a7828861558ef97c6ad4298c
Colin Blackburn <colb@bgs.ac.uk>
2024-07-03 10:17:54 +0100

    Use local font file for offline use

--- plugin/report_builder.py
+++ plugin/report_builder.py
@@ -58,6 +58,10 @@ class ReportBuilder:
         self.db_file = db_file
         self.report_filename = Path("field-report.html")
         self.css_filename = Path("style.css")
+        # Using locally downloaded woff2 of Google's Material Symbols Outlined font
+        # See: https://fonts.google.com/icons
+        # Licence: https://www.apache.org/licenses/LICENSE-2.0.html
+        self.font_filename = Path("MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].woff2")
 
 
     @property
@@ -82,6 +86,22 @@ class ReportBuilder:
         Get the ccs directory from the current project.
         """
         return self.project_dir / "css"
+
+
+    @property
+    def font_src_file(self) -> Path:
+        """
+        Get the font file path from the plugin folder.
+        """
+        return WORKDIR / "fonts" / self.font_filename
+
+
+    @property
+    def font_dest_dir(self) -> Path:
+        """
+        Get the ccs directory from the current project.
+        """
+        return self.project_dir / "fonts"
 
 
     @property
@@ -117,9 +137,11 @@ class ReportBuilder:
 
             with open(self.report_file, mode="w", encoding="utf-8") as report:
                 report.write(content)
-            # Copy CSS file to project directory
+            # Copy CSS and font files to project directory
             self.css_dest_dir.mkdir(parents=True, exist_ok=True)
+            self.font_dest_dir.mkdir(parents=True, exist_ok=True)
             shutil.copy(self.css_src_file, self.css_dest_dir / self.css_filename)
+            shutil.copy(self.font_src_file, self.font_dest_dir / self.font_filename)
 
             QMessageBox.information(None, "Information", f"Created field report:\n\n{self.report_file}")
         except Exception as exc:
