1580f94e2b523fe05967123ffddbe129c9488adf
Leo Rudczenko <leorud@bgs.ac.uk>
2024-08-20 09:48:47 +0100

    Update utils tests

--- test/conftest.py
+++ test/conftest.py
@@ -6,7 +6,11 @@ from unittest.mock import Mock
 
 import pytest
 import etlhelper as etl
-from qgis.core import QgsProject
+from qgis.core import (
+    QgsGeometry,
+    QgsFeature,
+    QgsProject,
+)
 from qgis.gui import QgsAdvancedDigitizingDockWidget
 from qgis.PyQt.QtWidgets import QMessageBox
 from qgis.testing.mocked import get_iface
@@ -98,6 +102,16 @@ def locality_point_count(fdc: FieldDataCapture) -> int:
         row_factory=etl.row_factories.tuple_row_factory,
     )[0]
     return row_count
+
+
+def create_empty_geometry_feature(wkt_str: str) -> QgsFeature:
+    """
+    Create a new and empty QgsFeature, with just a geometry made from the given WKT string.
+    """
+    geometry = QgsGeometry.fromWkt(wkt_str)
+    geometry_feature = QgsFeature()
+    geometry_feature.setGeometry(geometry)
+    return geometry_feature
 
 
 @pytest.fixture()
@@ -254,3 +268,13 @@ def report_builder(fdc_project: FieldDataCapture) -> ReportBuilder:
     report_builder = ReportBuilder()
 
     return report_builder
+
+
+@pytest.fixture()
+def empty_geometry_feature_polygon() -> QgsFeature:
+    """
+    This is mainly used for QuickMapTools where the tool returns an empty feature with just a geometry.
+    But the actual geometry can also be used for testing the creation of Polygon features.
+    To get the geometry from the feature, call feature.geometry().
+    """
+    return create_empty_geometry_feature("Polygon ((-3.06646639970546664 56.02224055154277949, -0.86620852862676745 52.89687413861690857, -1.3338961920444623 52.75580097369699217, -3.55541259327851167 55.88561238892003047, -3.06646639970546664 56.02224055154277949))")  # noqa
--- test/test_utils.py
+++ test/test_utils.py
@@ -2,10 +2,15 @@ from typing import Any
 
 import pytest
 
+from qgis.core import (
+    QgsGeometry,
+    QgsProject,
+)
 from qgis.PyQt.QtWidgets import QComboBox
 
 from plugin.field_data_capture import FieldDataCapture
 from plugin.utils import (
+    create_prepopulated_feature,
     get_table_rows,
     get_combobox_items_dict,
     set_combobox_index_by_data,
@@ -17,6 +22,60 @@ COMBOBOX_DATA = {
     "Item B": "item_b",
     "Item C": "item_c",
 }
+
+
+def test_validation_good(fdc_project: FieldDataCapture):
+    assert fdc_project.validate_qgis_state(
+        project_active=True,
+        db_file_exists=True,
+        fdc_layers_exist=True,
+        field_project_exists=True,
+    )
+
+
+def test_validation_bad(fdc: FieldDataCapture):
+    assert not fdc.validate_qgis_state(project_active=True, fdc_layers_exist=True)
+
+
+def test_check_field_project_exists(
+    fdc: FieldDataCapture,
+    qgs_project,
+    empty_geometry_feature_polygon,
+):
+    # Assert 1, the layer does not exist
+    assert not fdc.check_field_project_exists()
+
+    # Act 1, add fdc layers
+    fdc.add_gpkg_to_project()
+    fdc.add_gpkg_layers_to_project()
+
+    # Assert 2, the layer exists but has no features
+    assert not fdc.check_field_project_exists()
+
+    # Act 2, add an unsaved field_project
+    layer = QgsProject.instance().mapLayersByName("field_project")[0]
+    layer.startEditing()
+    # Create new feature
+    feature = create_prepopulated_feature(
+        layer,
+        prepopulate={
+            "short_name": "test_field_project",
+            "local_epsg": 27700,
+        },
+    )
+    # Add geometry
+    feature.setGeometry(empty_geometry_feature_polygon.geometry())
+    # Add feature to layer
+    layer.addFeature(feature)
+
+    # Assert 3, the layer exists with a feature but the feature is not saved
+    assert not fdc.check_field_project_exists()
+
+    # Act 3, save the feature so all the checks are good
+    layer.commitChanges()
+
+    # Assert 4, all the checks are good
+    assert fdc.check_field_project_exists()
 
 
 @pytest.fixture()
@@ -31,9 +90,11 @@ def combobox() -> QComboBox:
 
 
 @pytest.mark.parametrize(
-    "sql, count",
-    [("SELECT * FROM field_project", 1),
-     ("SELECT *, AsText(CastAutomagic(geometry)) as geom FROM locality_point", 2)]
+    ["sql", "count"],
+    (
+        ("SELECT * FROM field_project", 1),
+        ("SELECT *, AsText(CastAutomagic(geometry)) as geom FROM locality_point", 2),
+    ),
 )
 def test_get_rows(fdc_project: FieldDataCapture, sql: str, count: int):
     # Act
@@ -70,3 +131,29 @@ def test_set_combobox_index_by_data(
 
     # Assert
     assert (combobox.currentData() == data) is data_set
+
+
+@pytest.mark.parametrize(
+    ["layer_name", "prepopulate", "wkt"],
+    (
+        ("locality_point", {"locality_type_code": "outcrop", "map_face_note": "Honk"}, "Point (-3 55)"),
+        ("bedrock_line", {"line_label": "Quack"}, "LineString (-3 55, 55 -3)"),
+    ),
+)
+def test_create_prepopulated_feature(
+    layer_name: str,
+    prepopulate: dict[str, Any],
+    wkt: str,
+    fdc_project: FieldDataCapture,
+):
+    # Arrange
+    layer = QgsProject.instance().mapLayersByName(layer_name)[0]
+    geometry = QgsGeometry.fromWkt(wkt)
+
+    # Act
+    feature = create_prepopulated_feature(layer, prepopulate, geometry)
+
+    # Assert
+    for attribute, value in prepopulate.items():
+        assert feature.attribute(attribute) == value
+    assert feature.geometry().asWkt() == wkt
