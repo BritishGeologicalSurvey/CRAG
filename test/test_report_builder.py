de7ab41d028d82dbb2f9f5df29bcb6532fef2884
Colin Blackburn <colb@bgs.ac.uk>
2025-02-12 13:40:54 +0000

    Use report_builder mock consistently

--- test/test_report_builder.py
+++ test/test_report_builder.py
@@ -4,10 +4,8 @@ from bs4 import BeautifulSoup
 
 from conftest import locality_point_count
 
-from plugin.field_data_capture import FieldDataCapture
 from plugin.report_builder import ReportBuilder
 from plugin.utils import (  # noqa
-    FieldDataCaptureProject,
     ipdb_breakpoint,
 )
 
@@ -27,54 +25,54 @@ EXPECTED_CHILD_COLUMNS = {
 }
 
 
-def test_create_field_report(fdc_project: FieldDataCapture, monkeypatch_qmsgbox_question_yes):
+def test_create_field_report(report_builder: ReportBuilder, monkeypatch_qmsgbox_question_yes):
     # Act
-    success = fdc_project.create_field_report()
+    success = report_builder.create_field_report()
 
     # Assert
     assert success
 
 
-def test_create_html_field_report(fdc_project: FieldDataCapture, report_builder: ReportBuilder):
+def test_create_html_field_report(report_builder: ReportBuilder):
     # Act
     success = report_builder.create_html_field_report()
 
     # Assert
     assert success
     # Check file exists and is not empty
-    assert fdc_project.html_report_file.exists()
-    assert fdc_project.html_report_file.stat().st_size > 0
+    assert report_builder.html_report_file.exists()
+    assert report_builder.html_report_file.stat().st_size > 0
     # Confirm the correct number of sections has been created
-    soup = BeautifulSoup(fdc_project.html_report_file.read_text(encoding="utf-8"), 'lxml')
+    soup = BeautifulSoup(report_builder.html_report_file.read_text(encoding="utf-8"), 'lxml')
     project_sections = soup.findAll('section', {'class': "project"})
     assert len(project_sections) == 1
     locality_sections = soup.findAll('section', {'class': "locality_point"})
-    row_count = locality_point_count(fdc_project)
+    row_count = locality_point_count(report_builder)
     assert len(locality_sections) == row_count
     for child in EXPECTED_CHILD_COLUMNS.keys():
         child_sections = soup.findAll('section', {'class': child})
         assert len(child_sections) > 0
 
 
-def test_create_pdf_field_report(fdc_project: FieldDataCapture, report_builder: ReportBuilder):
+def test_create_pdf_field_report(report_builder: ReportBuilder):
     # Act
     success = report_builder.create_pdf_field_report()
 
     # Assert
     assert success
     # Check file exists and is not empty
-    assert fdc_project.pdf_report_file.exists()
-    assert fdc_project.pdf_report_file.stat().st_size > 0
+    assert report_builder.pdf_report_file.exists()
+    assert report_builder.pdf_report_file.stat().st_size > 0
 
 
-def test_get_report_data(fdc_project: FieldDataCapture, report_builder: ReportBuilder):
+def test_get_report_data(report_builder: ReportBuilder):
     # Act
     report_data = report_builder.get_report_data()
 
     # Assert
     assert report_data['project']
     # Check that the correct amount of data has been obtained
-    row_count = locality_point_count(fdc_project)
+    row_count = locality_point_count(report_builder)
     assert len(report_data['locality_points']) == row_count
     for locality in report_data['locality_points'].values():
         # Check that the children dict has been created
