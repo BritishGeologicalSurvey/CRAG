1869c32c9220dd87522b0a352c7a97c8aa48a1b8
Colin Blackburn <colb@bgs.ac.uk>
2025-02-13 09:22:38 +0000

    Use example from reportlab docs
    See: https://docs.reportlab.com/reportlab/userguide/ch9_other_useful_flowables/

--- plugin/report_builder.py
+++ plugin/report_builder.py
@@ -7,7 +7,14 @@ from jinja2 import (
     Environment,
     FileSystemLoader,
 )
-from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.styles import ParagraphStyle as PS
+from reportlab.platypus import PageBreak
+from reportlab.platypus.paragraph import Paragraph
+from reportlab.platypus.doctemplate import PageTemplate, BaseDocTemplate
+from reportlab.platypus.tableofcontents import TableOfContents
+from reportlab.platypus.frames import Frame
+from reportlab.lib.units import cm
+
 from qgis.core import (
     QgsCoordinateReferenceSystem,
     QgsCoordinateTransform,
@@ -50,6 +57,25 @@ CHILD_JOINS = {
                                "== child.third_attribute "),
     "superficial_landform": " JOIN dic_superficial_landform ON code == child.superficial_type_code ",
 }
+
+
+class MyDocTemplate(BaseDocTemplate):
+
+    def __init__(self, filename, **kw):
+        self.allowSplitting = 0
+        BaseDocTemplate.__init__(self, filename, **kw)
+        template = PageTemplate('normal', [Frame(2.5 * cm, 2.5 * cm, 15 * cm, 25 * cm, id='F1')])
+        self.addPageTemplates(template)
+
+    def afterFlowable(self, flowable):
+        "Registers TOC entries."
+        if flowable.__class__.__name__ == 'Paragraph':
+            text = flowable.getPlainText()
+            style = flowable.style.name
+            if style == 'Heading1':
+                self.notify('TOCEntry', (0, text, self.page))
+            if style == 'Heading2':
+                self.notify('TOCEntry', (1, text, self.page))
 
 
 class ReportBuilder(FieldDataCaptureProject):
@@ -95,9 +121,28 @@ class ReportBuilder(FieldDataCaptureProject):
                 if result == QMessageBox.No:
                     return False
 
-            canvas = Canvas(str(self.pdf_report_file))
-            canvas.drawString(72, 72, "Hello, World!")
-            canvas.save()
+            h1 = PS(name='Heading1', fontSize=14, leading=16)
+
+            h2 = PS(name='Heading2', fontSize=12, leading=14, leftIndent=10)
+
+            # Build story.
+            story = []
+            toc = TableOfContents()
+            # For conciseness we use the same styles for headings and TOC entries
+            toc.levelStyles = [h1, h2]
+            story.append(toc)
+            story.append(PageBreak())
+            story.append(Paragraph('First heading', h1))
+            story.append(Paragraph('Text in first heading', PS('body')))
+            story.append(Paragraph('First sub heading', h2))
+            story.append(Paragraph('Text in first sub heading', PS('body', leftIndent=10)))
+            story.append(PageBreak())
+            story.append(Paragraph('Second sub heading', h2))
+            story.append(Paragraph('Text in second sub heading', PS('body', leftIndent=10)))
+            story.append(Paragraph('Last heading', h1))
+
+            doc = MyDocTemplate(str(self.pdf_report_file))
+            doc.multiBuild(story)
 
         except Exception as exc:
             msg = ""
