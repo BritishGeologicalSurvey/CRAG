0e0143a8adacdee1811b16b9d0e641a7dc5a9cec
Colin Blackburn <colb@bgs.ac.uk>
2025-02-19 08:53:54 +0000

    Use templates for generic tables

--- plugin/pdf_content.py
+++ plugin/pdf_content.py
@@ -7,12 +7,30 @@ from reportlab.platypus.frames import Frame
 from reportlab.lib.units import cm
 
 
+PROJECT_TABLE = {
+    'project_lead': 'Project lead',
+    'start_date': 'Start date',
+    'end_date': 'End date',
+    'description': 'Description',
+    'notes': 'Notes'
+}
+
+LOCALITY_POINT_TABLE = {
+    'locality_type_code': 'Locality type',
+    'pdf_geometry': 'Location',
+    'locality_description': 'Locality description',
+    'map_face_note': 'Map face note',
+    'geology_description': 'Geology description'
+}
+
+
 class ReportTemplate(BaseDocTemplate):
     h1 = PS(name='Heading1', fontSize=18, spaceAfter=18)
     h2 = PS(name='Heading2', fontSize=16, spaceAfter=16)
     h3 = PS(name='Heading3', fontSize=14, spaceAfter=8)
     toc_entry = PS(name='TOCLocalityPoint', fontSize=12, spaceAfter=6)
     table_text = PS(name='TableTText', fontSize=12, spaceAfter=6)
+    report = []
 
     def __init__(self, filename, **kw):
         self.allowSplitting = 0
@@ -30,51 +48,50 @@ class ReportTemplate(BaseDocTemplate):
             if style == 'Heading3':
                 self.notify('TOCEntry', (1, text, self.page))
 
-    def get_locality_point_table(self, locality_point):
+    def append_table(self, data, fields):
         """
-        Build and return table with locality point data
+        Build and return table with data using fields as a template
         """
-        table_data = [
-            ['Locality type',
-                Paragraph(locality_point['locality_type_code'])],
-            ['Location',
-                Paragraph(locality_point['pdf_geometry'])],
-            ['Locality description',
-                Paragraph(locality_point['locality_description'])],
-            ['Map face note',
-                Paragraph(locality_point['map_face_note'])],
-            ['Geology description',
-                Paragraph(locality_point['geology_description'])],
-            ['Entered',
-                Paragraph(locality_point['user_entered'] + ' at ' + locality_point['date_entered'])],
+        table_data = []
+        for field, title in fields.items():
+            row = [title, Paragraph(data[field])]
+            table_data.append(row)
+        entered = [
+            'Entered',
+            Paragraph(data['user_entered'] + ' at ' + data['date_entered'])
         ]
-        table_style = [('GRID', (0, 0), (2, 6), 1, colors.blue),
-                       ('VALIGN', (0, 0), (2, 6), 'TOP')]
+        table_data.append(entered)
+
+        # Define and style the table
+        rows = len(table_data)
+        table_style = [('GRID', (0, 0), (2, rows), 0.5, colors.black),
+                       ('VALIGN', (0, 0), (2, rows), 'TOP')]
         table = Table(table_data,
                       colWidths=[5 * cm, 12 * cm],
                       style=table_style,
                       hAlign='LEFT',
                       spaceBefore=6,
                       spaceAfter=12)
-        return table
+
+        self.report.append(table)
 
     def render(self, content):
         """
         Build the full report
         """
-        report = []
         toc = TableOfContents()
         toc.levelStyles = [self.h2, self.toc_entry]
 
-        report.append(toc)
-        report.append(PageBreak())
-        report.append(Paragraph('Field Report: ' + content['project']['title'], self.h1))
-        report.append(Paragraph('Project information', self.h2))
+        self.report.append(toc)
+        self.report.append(PageBreak())
+        self.report.append(Paragraph('Field Report: ' + content['project']['title'], self.h1))
 
-        report.append(Paragraph('Locality Points', self.h2))
+        self.report.append(Paragraph('Project information', self.h2))
+        self.append_table(content['project'], PROJECT_TABLE)
+
+        self.report.append(Paragraph('Locality Points', self.h2))
         for locality_point_key, locality_point in content['locality_points'].items():
-            report.append(Paragraph('Locality point: ' + locality_point_key, self.h3))
-            table = self.get_locality_point_table(locality_point)
-            report.append(table)
+            self.report.append(Paragraph('Locality point: ' + locality_point_key, self.h3))
+            self.append_table(locality_point, LOCALITY_POINT_TABLE)
 
-        self.multiBuild(report)
+        self.multiBuild(self.report)
