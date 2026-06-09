61a0ff143f1caf887959cc4a4ca009217f286899
Leo Rudczenko <leorud@bgs.ac.uk>
2026-06-09 14:53:25 +0100

    Add new plugin metadata script

---
+++ bin/update_plugin_metadata.py
@@ -0,0 +1,47 @@
+"""Script to update the plugin metadata file with the version number."""
+import argparse
+import subprocess
+from configparser import ConfigParser
+
+
+def update_metadata(metadata_file):
+    """
+    Recreate the metadata file with an updated version number.
+    """
+    metadata = ConfigParser()
+    metadata.read(metadata_file)
+    metadata['general']['version'] = _get_version()
+    with open(metadata_file, 'wt') as f:
+        metadata.write(f)
+
+
+def get_zip_file_name_env():
+    """
+    Return zip file name filename.
+    """
+    tag = _get_version()
+    filename_env = f'PLUGIN_FILE_NAME=field_data_capture_{tag}.zip'
+    return filename_env
+
+
+def _get_version():
+    """
+    Return version number based on tag.
+    :return:
+    """
+    result = subprocess.run(['git', 'tag', '--points-at', 'HEAD'],
+                            check=True, capture_output=True, text=True)
+    tag = result.stdout.strip()
+    return tag
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Update the plugin metadata.txt and config file version.")
+    parser.add_argument("metadata_file", help="Location of metadata.txt",
+                        type=str)
+    args = parser.parse_args()
+
+    update_metadata(args.metadata_file)
+    filename_env = get_zip_file_name_env()
+    print(filename_env)
