7ac2d68eb1d152407a871f8939a88a7caf2be5d2
Colin Blackburn <colb@bgs.ac.uk>
2026-07-21 11:10:35 +0100

    Use warning with further details button

--- CRAG/crag.py
+++ CRAG/crag.py
@@ -65,6 +65,7 @@ from qgis.PyQt.QtWidgets import (
     QAction,
     QMenu,
     QMessageBox,
+    QPushButton,
     QToolBar,
     QWidget,
 )
@@ -628,10 +629,18 @@ class Crag(CragProject):
         for layer in vector_layers:
             self.refresh_relation_reference_widgets(layer)
 
-        QMessageBox.information(
-            None, "Information",
-            "GeoPackage layers loaded.\n\nNow set field project boundary polygon and metadata.",
-        )
+        message = "GeoPackage layers loaded. Now set field project boundary polygon and metadata."
+        help_message = ("Draw a new field_project polygon:\n"
+                        "using a standard mouse left-click for each point\n"
+                        "and right-click to finish the polygon")
+
+        message_bar = self.iface.messageBar().createMessage("CRAG", message)
+        help_button = QPushButton(message_bar)
+        help_button.setText("Further Details")
+        help_button.pressed.connect(lambda: QMessageBox.information(None, "Information", help_message))
+        message_bar.layout().addWidget(help_button)
+        self.iface.messageBar().pushWidget(message_bar, Qgis.Warning)
+
         return True
 
 
